e0da2b51b0e475798680e1f128c1b240631dd730
Gianluca Zicca <gianluca.zicca@bestit.de>
2024-06-08 18:53:49 +0200

    init commit nuxt app and nestjs backend

---
+++ apps/webApp/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { data: test } = await useAsyncData('test', () => $fetch('http://localhost:3001/'), 'GET');
+console.log(test);
+</script>
+
+<template>
+  <div>
+    <NuxtWelcome />
+    <div class="" />
+  </div>
+</template>
